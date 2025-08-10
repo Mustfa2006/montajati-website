e4faa2e498f9a98f7c0ec5a59065e73504a11f3c
Mustfa2006 <www.mustfaabd2006@gmail.com>
2025-08-10 06:07:33 +0300

    6445654

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"72f2b18bb094f92f62a3113a8075240ebb59af
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "318527151"
+    serviceWorkerVersion: "1740962797"
   }
 });
